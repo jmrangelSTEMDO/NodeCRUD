6c045db104da1bf8b1a09f40617df9c62dc439d2
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-02-05 10:20:27 +0100

    codigo limpio mas o menos, cambio de /user a /usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,32 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../usuarios/usuarios')
 
-
-// Configura el cliente de Supabase
-
-// POST - Crear un nuevo usuario
-router.post('/user', async (req, res) => {
+router.post('/usuarios', async (req, res) => {
   try {
     await userController.createData(req, res, userController.supabase);
   } catch (error){
     console.error('error al insertar usuario:', error.message);
-  }
-});
-
-
-router.get('/', async (req, res) =>{
-  try{
-    await userController.readTodos(req, res);
-  }catch(error){
-    console.error("Error al leer la tabla:", error.message);
-  }
-});
-
-router.get('/pepito', async (req, res) =>{
-  try{
-    await userController.readTodosDecrypted(req, res);
-  }catch(error){
-    console.error("Error al leer la tabla:", error.message);
   }
 });
 
@@ -39,14 +18,21 @@ router.get('/list', async (req, res) =>{
   }
 });
 
+// router.get('/', async (req, res) =>{
+//   try{
+//     await userController.readTodos(req, res);
+//   }catch(error){
+//     console.error("Error al leer la tabla:", error.message);
+//   }
+// });
 
-router.put('/:id', async (req,res) => {
-  try{
-    await userController.updateData(id)
-  }catch(error){
-    console.error("Error al leer la tabla:", error.message);
-  }
-});
+// router.get('/pepito', async (req, res) =>{
+//   try{
+//     await userController.readTodosDecrypted(req, res);
+//   }catch(error){
+//     console.error("Error al leer la tabla:", error.message);
+//   }
+// });
 
 
 module.exports = router;
--- src/usuarios/usuarios.js
+++ src/usuarios/usuarios.js
@@ -3,10 +3,13 @@ dotenv.config();
 import { createClient } from "@supabase/supabase-js";
 import CryptoJS from 'crypto-js';
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const supabase_url = process.env.SUPABASE_URL;
+const supabase_key = process.env.SUPABASE_KEY;
+
 
 export const supabase = createClient(
-  'https://cmzgqyfvwnsxrbinxkjk.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtemdxeWZ2d25zeHJiaW54a2prIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODU3MjU5MCwiZXhwIjoyMDU0MTQ4NTkwfQ.DSRew9cWsqX22HZIR6JkxDr5F7lAdxQbT4R5XqYYaUA'
+  supabase_url,
+  supabase_key
 );
 
 // Clave secreta para la encriptación (guárdala de forma segura, no la expongas en el código)
@@ -14,88 +17,109 @@ const SECRET_KEY = process.env.ENCRYPTION_SECRET_KEY;
 
 
 function encryptData(data) {
-    // return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
-    return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
+  // return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
+  return CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
 }
 
 export async function createData(req, res) {
-  // console.log(req.body.email)
-  // console.log(validarEmail(req.body.email))
-
-  if (validarEmail(req.body.email)){
-
+  if (validarEmail(req.body.email)) {
     try {
-      
       const encryptedData = encryptData(req.body.email);
-      // if(encryptData != "")
       const { data, error } = await supabase
-      .from('usuarios')
-      .insert([{ email: encryptedData }])
-      .select();
-      
+        .from('usuarios')
+        .insert([{ email: encryptedData }])
+        .select();
       if (error) throw error;
       return res.status(201).json({ message: 'Datos encriptados insertados correctamente' });
     } catch (error) {
       console.error('Error al insertar usuario:', error.message);
       return res.status(400).json({ error: error.message });
     }
-  }else{
+  } else {
     return res.status(400).json({ error: 'El correo electrónico introducido no es válido.' });;
   }
 }
 
 export async function readTodos(req, res) {
-    try{
+  try {
     const { data, error } = await supabase
       .from('usuarios')
       .select('*');
-  
+
     if (error) throw error;
-    return res.status(201).json({data})
-  } catch(error){
+    return res.status(201).json({ data })
+  } catch (error) {
     console.error("Error al leer la tabla:", error.message);
-    return res.status(400).json({error: error.message});
+    return res.status(400).json({ error: error.message });
   }
 }
 
 
 export async function readTodosDecrypted(req, res) {
-    try{
+  try {
     const { data, error } = await supabase
       .from('usuarios')
       .select('*');
 
-    let lista = [{}];
+    let lista = [];
     data.forEach(element => {
-        let emailD = CryptoJS.AES.decrypt(element.email, SECRET_KEY).toString(CryptoJS.enc.Utf8);
-    
-        lista.push({
-            id: element.id,
-            email: emailD,
-            fechaRegistro: element.fechaRegistro
-        });
-        
+      let emailD = CryptoJS.AES.decrypt(element.email, SECRET_KEY).toString(CryptoJS.enc.Utf8);
+
+      lista.push({
+        id: element.id,
+        email: emailD,
+        fechaRegistro: element.fechaRegistro
+      });
+
     });
-  
+
     if (error) throw error;
-    return res.status(201).json({lista})
-  } catch(error){
+    return res.status(201).json({ lista })
+  } catch (error) {
     console.error("Error al leer la tabla:", error.message);
-    // console.log(data);
-    return res.status(400).json({error: error.message});
+    return res.status(400).json({ error: error.message });
   }
 }
 
+export async function csv(req, res) {
+  let json = [];
+  try {
+    const { data, error } = await supabase
+      .from('usuarios')
+      .select('*');
+    data.forEach(element => {
+      let emailD = CryptoJS.AES.decrypt(element.email, SECRET_KEY).toString(CryptoJS.enc.Utf8);
+
+      json.push({
+        id: element.id,
+        email: emailD,
+        fechaRegistro: element.fechaRegistro
+      });
+
+    });
+
+  } catch (e) {
+
+  }
+  json = JSON.stringify(json);
+  let csv = jsonToCsv(json);
+
+  csv = CryptoJS.AES.encrypt(csv, SECRET_KEY).toString();
+  res.header('Content-Type', 'text/csv');
+  res.attachment('datos.csv');
+  res.send(csv);
+
+}
 
 // export async function updateData(req, res, id) {
 //   try {
-  
+
 //     const encryptedData = encryptData(req.body.email);
 //     const { data, error } = await supabase
 //       .from('usuarios')
 //       .update([{ email: encryptedData }])
 //       .eq('id', id);
-    
+
 //     if (error) throw error;
 //     return res.status(201).json({ message: 'Datos encriptados actualizados correctamente' });
 //   } catch (error) {
@@ -104,43 +128,6 @@ export async function readTodosDecrypted(req, res) {
 //   }
 // }
 
-export async function csv(req, res) {
-  let  json = [{}];
-  try{
-    const { data, error } = await supabase
-    .from('usuarios')
-    .select('*');
-    // console.log(data);
-    // json = data;
-    //esto duplicaba las lineas.
-    data.forEach(element => {
-        let emailD = CryptoJS.AES.decrypt(element.email, SECRET_KEY).toString(CryptoJS.enc.Utf8);
-    
-        json.push({
-            id: element.id,
-            email: emailD,
-            fechaRegistro: element.fechaRegistro
-        });
-        
-    });
-
-  }catch (e){
- 
-  }
-  json = JSON.stringify(json);
-  //el problema erradica en que el json ya tiene los dos duplicados
-  console.log(json);
-  let csv = jsonToCsv(json);
-
-  csv = CryptoJS.AES.encrypt(csv, SECRET_KEY).toString();
-  res.header('Content-Type', 'text/csv');
-  res.attachment('datos.csv');
-  // console.log(csv);
-
-  // console.log(CryptoJS.AES.decrypt(csv, SECRET_KEY).toString(CryptoJS.enc.Utf8));
-  res.send(csv);
-  
-}
 
 function jsonToCsv(json) {
   const items = JSON.parse(json);
