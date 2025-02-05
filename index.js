cd5c0df935d8c863acadae87b41ae0b36b5b718c
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-02-05 11:23:06 +0100

    /

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var allowedOrigins = [
   'http://localhost:4200/inicio',
   'http://localhost:8000',
   'http://192.168.88.167',
-  'https://loginmicrosoftonlinecom-git-master-sergios-projects-d4c71fde.vercel.app/',
+  'https://loginmicrosoftonlinecom-git-master-sergios-projects-d4c71fde.vercel.app',
   'http://192.168.88.162'
 ];
 app.use(cors({
