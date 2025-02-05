55baffbff9dda1f3aa261e34de819675e9bb9e4a
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-02-05 11:25:34 +0100

    depurasion

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ var allowedOrigins = [
 ];
 app.use(cors({
   origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
+    console.log('Origin:', origin); 
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
