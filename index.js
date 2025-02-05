d3f03c1b44e5a63a4dce6e9c3d77534c69153177
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-02-05 14:50:11 +0100

    origin: true

--- index.js
+++ index.js
@@ -33,16 +33,8 @@ var allowedOrigins = [
 // Middleware para parsear JSON
 
 app.use(cors({
-  origin: function(origin, callback){
-    console.log('Origin:', origin); 
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: true,
+  credentials: true
 }));
 
 
